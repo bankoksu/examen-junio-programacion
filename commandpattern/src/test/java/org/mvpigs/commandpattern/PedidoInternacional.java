603a08fa15f03c50a944952057cf2dd4b59f2f44
Dual <seshomarualex@gmail.com>
2018-06-07 16:50:21 +0200

    primera subida luego de ordenar un poco las carpetas

---
+++ commandpattern/src/test/java/org/mvpigs/commandpattern/PedidoInternacional.java
@@ -0,0 +1,17 @@
+package org.mvpigs.commandpattern;
+
+import org.mvpigs.commandpattern.interfaces.Pedido;
+
+public class PedidoInternacional implements Pedido {
+
+	public int peso() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String destino() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
